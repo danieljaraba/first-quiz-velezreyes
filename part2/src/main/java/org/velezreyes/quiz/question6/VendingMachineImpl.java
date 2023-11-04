600027afd2d4decfba3c3843781111976638536b
danieljaraba <daesjaga2003@gmail.com>
2023-11-03 23:40:27 -0500

    Part2 completed

--- part2/src/main/java/org/velezreyes/quiz/question6/VendingMachineImpl.java
+++ part2/src/main/java/org/velezreyes/quiz/question6/VendingMachineImpl.java
@@ -1,9 +1,46 @@
 package org.velezreyes.quiz.question6;
 
-public class VendingMachineImpl {
+public class VendingMachineImpl implements VendingMachine{
+
+  private double balance;
+
+  public VendingMachineImpl() {
+    this.balance = 0;
+  }
 
   public static VendingMachine getInstance() {
     // Fix me!
-    return null;
+    return new VendingMachineImpl();
+  }
+
+  @Override
+  public void insertQuarter() {
+    balance += 0.25;
+  }
+
+  @Override
+  public Drink pressButton(String name) throws NotEnoughMoneyException, UnknownDrinkException {
+    if (balance == 0) {
+      throw new NotEnoughMoneyException();
+    }
+
+    switch (name) {
+      case "ScottCola":
+        if (balance >= 0.75) {
+          balance -= 0.75;
+          return new DrinkImpl("ScottCola", true);
+        } else {
+          throw new NotEnoughMoneyException();
+        }
+      case "KarenTea":
+        if (balance >= 1.00) {
+          balance -= 1.00;
+          return new DrinkImpl("KarenTea", false);
+        } else {
+          throw new NotEnoughMoneyException();
+        }
+      default:
+        throw new UnknownDrinkException();
+    }
   }
 }
